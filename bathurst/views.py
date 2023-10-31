fd68b2bc25b3495313b6f7e068b98a7424d9767f
ednganji <ednganji01@monccnb.ca>
2023-10-31 11:08:47 -0300

    avancée

--- bathurst/views.py
+++ bathurst/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 import re
 from django.http import HttpResponse
+from bathurst.models import Capteurs
 
 
 def home(request):
-   
-    return render(request, "bathurst/index.html")
+    #actu = Capteurs.objects.get(id=max(id))
+    actu = Capteurs.objects.last()
+    return render(request, "bathurst/index.html", {'actu': actu})
 
 def historique(request):
-   
-    return render(request, "bathurst/historique.html")
+    hist = Capteurs.objects.all().order_by('-id')[:20]
+    return render(request, "bathurst/historique.html", 
+                  {'hist': hist})
 
 
